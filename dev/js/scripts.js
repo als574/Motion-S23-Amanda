c2f1716ae2822e5b4d28dd7fa401eb4f6ccbfff9
als574 <asilverman314@gmail.com>
2023-04-19 23:41:45 -0400

    anim added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,4 +3,14 @@ import { gsap } from "gsap";
 
 //gsap.to(console.log("#logo", {duration:3, opacity:0.5, delay:1}));
 
-gsap.to("button", {duration:1, alpha:1});
+gsap.to("#logo", {duration:0.5, alpha:1, delay:0.5});
+gsap.to("nav", {duration:0.5, y: "-=25"});
+
+gsap.to("#hero-content", {duration:0.75, y: "-=30", stagger:0.5});
+
+gsap.to("#content", {duration:.25, alpha:0.75});
+
+ 
+
+
+
