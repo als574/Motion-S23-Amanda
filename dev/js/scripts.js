5114289a43292d52788404f0854509cd3c35d2c8
als574 <asilverman314@gmail.com>
2023-06-08 06:03:33 -0400

    anim edit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,15 +1,15 @@
 import { gsap } from "gsap";
-//import { MotionPathPlugin } from "gsapMotionPathPlugin"; // Update the file path
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"; // Update the file path
-//mport { GSDevTools } from "gsapGSDevTools"; // Update the file path
+//import { MotionPathPlugin } from "gsap/MotionPathPlugin"; 
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"; 
+import { GSDevTools } from "gsap/GSDevTools"; 
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools); //MotionPathPlugin
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 let mainTL = gsap.timeline();
 
 function letterO(){
     let tl = gsap.timeline();
     
-        tl.from("#o", {duration: 1, scale: 3, x: "-=450%"});
+        tl.from("#o", {duration: 2, scale: 75, x: "+=450%",y: "-=500%"});
     
       return tl;
     }
@@ -17,7 +17,7 @@ function letterO(){
     function letterI(){
         let tl = gsap.timeline();
         
-            tl.from("#i", {duration: 1, scale: 3, x: "+=450%"});
+            tl.from("#i", {duration: 2, scale: 3, x: "+=450%"},"+=0.25");
         
           return tl;
         }
@@ -25,7 +25,7 @@ function letterO(){
         function letterN(){
             let tl = gsap.timeline();
             
-                tl.from("#n", {duration: 1, scale: 3, x: "-=450%"});
+                tl.from("#n", {duration: 1, scale: 3, x: "-=450%"},"+=0.75");
             
               return tl;
             }
@@ -33,14 +33,23 @@ function letterO(){
             function letterR(){
                 let tl = gsap.timeline();
                 
-                    tl.from("#r", {duration: 1, scale: 3, x: "-=450%", y:"-=100%"});
+                    tl.from("#r", {duration: 1, scale: 3, x: "+=450%", y:"-=400%"},"+=1");
                 
                   return tl;
                 }
 
+                function pig(){
+                    let tl = gsap.timeline();
+                    
+                        tl.from("#Pig", {duration: 1, x: "-=350%"},"+=1");
+                    
+                      return tl;
+                    }
+
 mainTL.add(letterO())
     .add(letterI(), 0)
     .add(letterN(), 0)
-    .add(letterR(), 0);
+    .add(letterR(), 0)
+    .add(pig());
 
 GSDevTools.create();
