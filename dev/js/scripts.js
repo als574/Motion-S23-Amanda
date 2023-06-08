29f27b6a77a6090b7e5566b6f260a9ca82672a1c
als574 <asilverman314@gmail.com>
2023-06-08 05:53:21 -0400

    primary anim added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,46 @@
 import { gsap } from "gsap";
-import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin"; // Update the file path
-import { DrawSVGPlugin } from "gsap/dist/DrawSVGPlugin"; // Update the file path
-import { GSDevTools } from "gsap/dist/GSDevTools"; // Update the file path
+//import { MotionPathPlugin } from "gsapMotionPathPlugin"; // Update the file path
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"; // Update the file path
+//mport { GSDevTools } from "gsapGSDevTools"; // Update the file path
 
-gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
-
-
-
-//return tl;
-//}
-
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools); //MotionPathPlugin
 let mainTL = gsap.timeline();
-//mainTL.add(opacityAnimation());
+
+function letterO(){
+    let tl = gsap.timeline();
+    
+        tl.from("#o", {duration: 1, scale: 3, x: "-=450%"});
+    
+      return tl;
+    }
+
+    function letterI(){
+        let tl = gsap.timeline();
+        
+            tl.from("#i", {duration: 1, scale: 3, x: "+=450%"});
+        
+          return tl;
+        }
+
+        function letterN(){
+            let tl = gsap.timeline();
+            
+                tl.from("#n", {duration: 1, scale: 3, x: "-=450%"});
+            
+              return tl;
+            }
+
+            function letterR(){
+                let tl = gsap.timeline();
+                
+                    tl.from("#r", {duration: 1, scale: 3, x: "-=450%", y:"-=100%"});
+                
+                  return tl;
+                }
+
+mainTL.add(letterO())
+    .add(letterI(), 0)
+    .add(letterN(), 0)
+    .add(letterR(), 0);
 
 GSDevTools.create();
