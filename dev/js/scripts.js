77c2d79ba105d2319d7b20894648c49e7c94236e
als574 <asilverman314@gmail.com>
2023-06-14 17:59:47 -0400

    scale changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -20,9 +20,10 @@ function nBlue() {
   let tl = gsap.timeline();
 
   tl.from("#nblue1", { visibility: "hidden", fill: "#2C344C" })
-    .from("#nblue1", { duration: 1, scale: 0.25 })
+    .from("#nblue1", { duration: 1, scale: 0.25, transformOrigin:"center"})
     .to("#nblue1", { visibility: "visible" })
-    .to("#nblue1", {duration: 2, scale: 1.25, rotate: -60, x: "-=450%",y: "-=100%"});
+    .to("#nblue1", {duration: 2, scale: 1.25, rotate: -60, x: "-=100%",y: "-=100%"})
+    .to("#nblue1", {duration: 1.25, rotate: -10, x: "-=100%"});
 
   return tl;
 }
@@ -30,21 +31,23 @@ function nBlue() {
 function red() {
   let tl = gsap.timeline();
 
-  tl.from("#red1", {visibility: "hidden", fill: "#CD4225", transformOrigin: "50% 50%"})
-    .from("#red1", { duration: 0.75, scale: 0.005 })
-    .to("#red1", { visibility: "visible" })
-    .to("#red1", {duration: 2,scale: 1.005, rotate: 60, x: "+=450%", y: "-=100%"});
-
+  tl.from("#red1", {visibility: "hidden", fill: "#CD4225"})
+    .from("#red1", {duration: 1, scale: 0.15, transformOrigin:"center", x:-490, y:-80})
+    .to("#red1", {visibility: "visible"})
+    .to("#red1", {duration: 0.5, rotate: -90})
+    .to("#red1", {duration: 2, scale: 0, x: "+=450%", y: "-=225%"});
+  
   return tl;
 }
 
 function yellow() {
   let tl = gsap.timeline();
 
-  tl.from("#yellow1", { visibility: "hidden", fill: "#2C344C" })
-    .from("#yellow1", { duration: 1, scale: 0.05 }, "=+0.05")
+    tl.set("#yellow1", {opacity: 0.85})
+    .from("#yellow1", { visibility: "hidden", fill: "#2C344C" },"+=0.05")
+    .from("#yellow1", { duration: 1.5, scale: 0.05 })
     .to("#yellow1", { visibility: "visible" })
-    .to("#yellow1", {duration: 2,scale: 1.25, rotate: 60, x: "-=450%", y: "+=25%"});
+    .to("#yellow1", {duration: 2, scale: 1.75, rotate:38, x: "-=250%", y: "-=175%"},"<");
 
   return tl;
 }
@@ -52,8 +55,12 @@ function yellow() {
 function lBlue() {
   let tl = gsap.timeline();
 
-  tl.from("#lblue1", {visibility: "hidden",  fill: "#CD4225", transformOrigin: "50% 50%"})
-    .from("#lblue1", {duration: 0.75, scale: 0.15});
+  tl.set("#lblue1", {opacity: 0.85})
+    .from("#lblue1", {visibility: "hidden",  fill: "#CD4225", transformOrigin: "50% 50%"},"+=0.15")
+    .from("#lblue1", {duration: 1.5, opacity: 0.5,scale: 0.15, x: "-=152%", y: "-=60%"})
+    .to("#lblue1", { visibility: "visible" })
+    .to("#lblue1", {duration: 2, scale: 2, rotate:-30, x: "+=130%", y: "+=125%"},"<");
+
     return tl;
   }
 
