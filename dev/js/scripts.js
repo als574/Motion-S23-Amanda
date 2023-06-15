a554e7c98e70c80b7a66a5c5b31c0b979e781ebe
als574 <asilverman314@gmail.com>
2023-06-15 18:54:50 -0400

    morph added

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,15 +1,16 @@
 import { gsap } from "gsap";
 //import { MotionPathPlugin } from "gsap/MotionPathPlugin"; 
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin"; 
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools"; 
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin, GSDevTools);
 let mainTL = gsap.timeline();
 
 function letterO(){
     let tl = gsap.timeline();
     
-        tl.from("#o", {duration: 2, scale: 75, x: "+=450%",y: "-=500%"});
+        tl.from("#o", {duration: 5, scale: 75, x: "+=450%",y: "-=500%"});
     
       return tl;
     }
@@ -17,7 +18,7 @@ function letterO(){
     function letterI(){
         let tl = gsap.timeline();
         
-            tl.from("#i", {duration: 2, scale: 3, x: "+=450%"},"+=0.25");
+            tl.from("#i", { duration: 4, ease: "power3.in",scale: 8, x: "+=450%"},"+=0.25");
         
           return tl;
         }
@@ -25,7 +26,7 @@ function letterO(){
         function letterN(){
             let tl = gsap.timeline();
             
-                tl.from("#n", {duration: 1, scale: 3, x: "-=450%"},"+=0.75");
+                tl.from("#n", { duration: 2, ease: "power3.in",scale: 3, x: "-=450%"},"+=0.75");
             
               return tl;
             }
@@ -33,23 +34,37 @@ function letterO(){
             function letterR(){
                 let tl = gsap.timeline();
                 
-                    tl.from("#r", {duration: 1, scale: 3, x: "+=450%", y:"-=400%"},"+=1");
+                    tl.from("#r", { duration: 2, ease: "power3.in", x: "+=450%", y:"-=400%"},"+=1");
                 
                   return tl;
                 }
 
-                function pig(){
-                    let tl = gsap.timeline();
+                // function pig(){
+                //     let tl = gsap.timeline();
+                
+                //         tl.from("#Pig", { duration: 5, ease: "power3.in", x: "-=350%"})
+                //         .from("#Pig", {duration: 2, drawSVG:"20% 60%", stagger: 0.5})
+                        
+                //         .to("#Pig", {duration: 1, morphSVG:"Pig2"});
+                //       return tl;
+                //     }
+
+                function pig() {
+                  let tl = gsap.timeline();
+                  
+                  tl.from("#Pig2", {duration:1, skewY:7 },0)
+                    //tl.from("#Pig", { duration: 5, ease: "power3.in", x: "-=350%" })
+                    .from("#Pig", { duration: 2, drawSVG: "20% 60%", stagger: 0.5 })
+                    .to("#Pig", { duration: 1, morphSVG: "#Pig2" })
+                    .set("#Pig2", { visibility: "hidden" }); 
                     
-                        tl.from("#Pig", {duration: 1, x: "-=350%"},"+=1");
-                    
-                      return tl;
-                    }
+                  return tl;
+                }
 
 mainTL.add(letterO())
     .add(letterI(), 0)
     .add(letterN(), 0)
     .add(letterR(), 0)
-    .add(pig());
+    .add(pig(),0);
 
 GSDevTools.create();
